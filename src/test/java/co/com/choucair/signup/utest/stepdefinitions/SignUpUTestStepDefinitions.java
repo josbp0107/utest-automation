bf34ad14b55efa0c950035b23eb0dc3cee9cc30d
Jose Ballesteros <josebp0107@gmail.com>
2021-08-04 20:02:54 -0500

    Add StepDefinitions

--- src/test/java/co/com/choucair/signup/utest/stepdefinitions/SignUpUTestStepDefinitions.java
+++ src/test/java/co/com/choucair/signup/utest/stepdefinitions/SignUpUTestStepDefinitions.java
@@ -1,2 +1,37 @@
-package co.com.choucair.signup.utest.stepdefinitions;public class SignUpUTestStepDefinitions {
+package co.com.choucair.signup.utest.stepdefinitions;
+
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.serenitybdd.screenplay.actors.OnlineCast;
+
+public class SignUpUTestStepDefinitions {
+
+    // Sets scenario for our actor
+    @Before
+    public void setStage(){
+        OnStage.setTheStage(new OnlineCast());
+    }
+
+    @Given("^than juan wants to register first step at Utest$")
+    public void thanJuanWantsToRegisterFirstStepAtUtest() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @When("^he enters each data in the different inputs$")
+    public void heEntersEachDataInTheDifferentInputs() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^he fill all inputs and go next step$")
+    public void heFillAllInputsAndGoNextStep() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+
 }
