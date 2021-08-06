3062d08e6e1c8c04dfb8d5a8df630f379aff2c76
Jose Ballesteros <josebp0107@gmail.com>
2021-08-06 17:37:03 -0500

    Add methods to generate month, day and year

--- src/main/java/co/com/choucair/signup/utest/util/GenerateData.java
+++ src/main/java/co/com/choucair/signup/utest/util/GenerateData.java
@@ -18,11 +18,26 @@ public class GenerateData {
         return faker.internet().emailAddress();
     }
 
+    public  String month(){
+        int number = (int)(Math.random()*(12)+1);
+        return "number:"+number;
+    }
+
+    public String day(){
+        int number = (int)(Math.random()*(28)+1);
+        return "number:"+number;
+    }
+
+    public String year(){
+        int number = (int)(Math.random()*((2003 - 1872) + 1)) + 1872;
+        return "number:"+number;
+    }
+
     public String city(){
         return faker.country().capital();
     }
 
-    public String zipCode(){
+    public String zipCode() {
         return faker.address().zipCode();
     }
 
